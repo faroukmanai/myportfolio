a11a1db305a70d479a1077423b52d9c9520f2968
faroukmanai <79013591+faroukmanai@users.noreply.github.com>
2023-11-13 15:06:08 -0500

    change color in dark-mode

--- script.js
+++ script.js
@@ -30,47 +30,11 @@ TweenMax.from("svg",2, {
 
 document.addEventListener("DOMContentLoaded", function() {
 
-    // Fonction pour gérer le défilement fluide
-    function smoothScroll(event, targetId) {
-        event.preventDefault(); // Empêchez le comportement par défaut du lien
-        const targetSection = document.getElementById(targetId);
-        targetSection.scrollIntoView({
-            behavior: "smooth",
-            block: "start"
-        });
-    }
-
-    // Obtenez le lien "À propos" et "Projets"
-    const aboutLink = document.querySelector('a[href="#about"]');
-    const projectLink = document.querySelector('a[href="#projects"]');
-    const contactLink = document.querySelector('a[href="#contact"]');
-
-    // Ajoutez un écouteur d'événements
-    aboutLink.addEventListener("click", function(e) {
-        smoothScroll(e, "about");
-    });
-
-    // Écouteur d'événements pour "Projets"
-    projectLink.addEventListener("click", function(e) {
-        smoothScroll(e, "projects");
-    });
-
-    // Écouteur d'événements pour "contact"
-    contactLink.addEventListener("click", function(e) {
-        smoothScroll(e, "contact");
-    });
-
-
     // Pour le bouton modedark
     const button = document.querySelector("button");
     button.addEventListener("click", function() {
       document.body.classList.toggle("dark");
       
-      // Pour la navigation
-      document.querySelectorAll('nav ul li a').forEach((element) => {
-        element.classList.toggle('dark');
-      });
-  
       const svgText = document.querySelector('#svgText');
       if (document.body.classList.contains("dark")) {
         svgText.setAttribute('fill', 'url(#dark-wave)');
@@ -82,7 +46,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const githubIcon = document.querySelector(".github-icon img");
 
     if (document.body.classList.contains("dark")) {
-        
         linkedinIcon.style.background = "transparent";
         githubIcon.style.background = "transparent";
     } else {
@@ -125,12 +88,5 @@ document.addEventListener("DOMContentLoaded", function() {
             console.error("There was an error fetching the data:", error);
         });
 });
-// document.addEventListener("DOMContentLoaded", function() {
-//     // Pour le bouton
-//     const button = document.querySelector("button");
-//     button.addEventListener("click", function() {
-//       document.body.classList.toggle("dark");
-//     });
-    
-//   });
+
   
